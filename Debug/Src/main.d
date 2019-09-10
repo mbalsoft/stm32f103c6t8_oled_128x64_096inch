8980963d92c71538bad725e837b490e202c5a8f1
mbal <mbalhack@gmail.com>
2019-09-10 20:55:02 +0200

    v0.1 simple test of connection OLED display - works

---
+++ Debug/Src/main.d
@@ -0,0 +1,79 @@
+Src/main.o: ../Src/main.c \
+ C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Inc/main.h \
+ C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
+ C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Inc/stm32f1xx_hal_conf.h \
+ C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Inc/main.h \
+ C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
+ C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h \
+ C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h \
+ C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Drivers/CMSIS/Include/core_cm3.h \
+ C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h \
+ C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h \
+ C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h \
+ C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h \
+ C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h \
+ C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h \
+ C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h \
+ C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
+ C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
+ C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_i2c.h \
+ C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
+ C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Inc/ssd1306.h \
+ C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Inc/ssd1306_fonts.h \
+ C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Inc/ssd1306_tests.h
+
+C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Inc/main.h:
+
+C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
+
+C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Inc/stm32f1xx_hal_conf.h:
+
+C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Inc/main.h:
+
+C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h:
+
+C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h:
+
+C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h:
+
+C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h:
+
+C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Drivers/CMSIS/Include/core_cm3.h:
+
+C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Drivers/CMSIS/Include/core_cmInstr.h:
+
+C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Drivers/CMSIS/Include/core_cmFunc.h:
+
+C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
+
+C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h:
+
+C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h:
+
+C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h:
+
+C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h:
+
+C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h:
+
+C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h:
+
+C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h:
+
+C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h:
+
+C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_i2c.h:
+
+C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
+
+C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Inc/ssd1306.h:
+
+C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Inc/ssd1306_fonts.h:
+
+C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Inc/ssd1306_tests.h:
---
+++ Debug/Src/ssd1306_fonts.d
@@ -0,0 +1,4 @@
+Src/ssd1306_fonts.o: ../Src/ssd1306_fonts.c \
+ C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Inc/ssd1306_fonts.h
+
+C:/src/STM32/STM32F103C8T6minmum/OLED\ 0_96\ SSD1306/stm32_oled_096_ssd1306/Inc/ssd1306_fonts.h:
